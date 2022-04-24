57818c8a77c4c26c3b8ff265c3995b2347985b1c
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-04-24 06:38:31 +0000

    Delete footer.js

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-import {Text, VStack } from '@chakra-ui/react'
-
-const Footer = () => {
-      <VStack w="100%" h="100px" bg="gray.400">
-      <Text> 2022</Text>
-      </VStack>
-}
-
-export default Footer
