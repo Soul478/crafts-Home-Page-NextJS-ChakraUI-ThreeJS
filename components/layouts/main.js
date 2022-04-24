4eb471b7564da8152420867602f57ce1318dadd8
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-04-24 06:49:59 +0000

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 import NoSsr from '../no-ssr.js'
-import {Box, Container} from '@chakra-ui/react'
+import {Box, Container, Text, VStach} from '@chakra-ui/react'
 import VoxelDog from '../voxel-dog.js'
 
 const Main = ({children, router}) => {
@@ -21,6 +21,9 @@ const Main = ({children, router}) => {
                 {children}
             </Container>
         </Box>
+<VStack w="100%" h="30px" bg="gray.400">
+    <Text>Copyright 2022 | SpiriTech</Text>
+</VStack>
     )
 }
 
