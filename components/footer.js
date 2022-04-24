0c836da8441bb912cb32c28c9984c6a28e243b35
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-04-24 06:28:45 +0000

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,9 @@
 import {Text, VStack } from '@chakra-ui/react'
 
 const Footer = () => {
-    return (
       <VStack w="100%" h="100px" bg="gray.400">
       <Text> 2022</Text>
       </VStack>
-         )
 }
 
 export default Footer
