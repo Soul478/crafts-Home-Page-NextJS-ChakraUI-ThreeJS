f7eb4eee0eabc1a4802737067fe6644a2e782d2a
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-05-05 16:49:26 +0000

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,6 +10,7 @@ const Main = ({children, router}) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
                 <title>CHABANA Mohamed Amin - Homepage</title>
             </Head>
 
