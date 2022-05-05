bd3a083d51aa19ce624bdac946d54d4e02b31f86
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-05-05 15:35:28 +0000

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontSize="sm">
+    <Box align="center" opacity={0.4} fontSize="sm" height="bottom">
       &copy; {new Date().getFullYear()} CHABANA Mohamed Amin. All Rights Reserved.
     </Box>
   )
