812de3044d8ba532e7129a1270f131c989bcc2d4
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-05-29 14:38:03 +0100

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -11,7 +11,7 @@ const Main = ({children, router}) => {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-                <title>CHABANA Mohamed Amin - Homepage</title>
+                <title>CHABANA Mohamed Amin - Home</title>
             </Head>
 
             <Navbar path={router.asPath} />
