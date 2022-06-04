2217fa0bc1a5e103b2e8cfd927cc90ce5dca0bff
Soul478 <90095841+Soul478@users.noreply.github.com>
2022-06-04 17:13:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ const Page = () => {
                         maxWidth="123px"
                         display="inline-block"
                         borderRadius="full"
-                        src="/images/Amin_.jpg"
+                        src="/images/Amin_.JPG"
                         alt="Profile Image"
                         />
                     </Box>
